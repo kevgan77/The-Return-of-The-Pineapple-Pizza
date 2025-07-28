49e56d9359d781eacfb877bb2ee86abad20f6527
Gablinski <gabsil37@kingshigh.school.nz>
2025-07-28 12:24:30 +1200

    player movement faster

--- player.gd
+++ player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-@export var SPEED = 85
-@export var SPRINT_MULTIPLIER = 1.5
-@export var ACCELERATION = 20.0
-@export var FRICTION = 10.0
+@export var SPEED = 120
+@export var SPRINT_MULTIPLIER = 2.0
+@export var ACCELERATION = 10
+@export var FRICTION = 15.0
 @onready var sprite = $AnimatedSprite2D
 
 func play_animation_direction():
